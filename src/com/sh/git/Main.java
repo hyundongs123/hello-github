54e265cb7a27ef18c39f45a4b87b9b1810511c46
hyundongs123 <112843717+hyundongs123@users.noreply.github.com>
2022-12-19 16:08:19 +0900

    111

--- src/com/sh/git/Main.java
+++ src/com/sh/git/Main.java
@@ -5,7 +5,7 @@ package com.sh.git;
  * - revert : 지정한 커밋의 작업내용을 취소한 버젼을 새로운 커밋으로 만든다. (conflict날수 있음)
  * 
  * - reset : 지정한 커밋으로 돌아간다.
- *   - hard : 지정 커밋 이후 변경사항 버림
+ *   - hard : 지정 커밋 이후 변경사항 버림1111111111111111111111
  *   - mixed : 지정 커밋 이후 변경사항을 unstaging area 보관. 재추가후 커밋 가능
  *   - soft : 지정 커밋 이후 변경사항을 staging area 보관. 즉시 커밋 가능
  * 
